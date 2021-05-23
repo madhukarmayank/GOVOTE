54d758e8ef632ede3310bce9b5c4d8b4436d6a63
Visinigiri Aditya <61955750+adityakumar48@users.noreply.github.com>
2021-05-23 22:49:53 +0530

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 	<html>
 	<head>
-		<title></title>
+		<title>Admin Login</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
